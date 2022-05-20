250d14a7e50dfec3b422825717177fb8d8772657
Vladislav <vladisl13777@gmail.com>
2022-05-20 10:55:44 +0300

    second commit

--- script.js
+++ script.js
@@ -6,4 +6,4 @@
 
 const answer = prompt('Сколько вам лет ?', ''); 
 console.log(typeof(answer)); // Вся инфа, которая идет от пользователя - это есть строка
-document.write('lddlasdl');
+document.write('lddlasdкl'); 
