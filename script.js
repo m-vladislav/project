70695f813892c26816c4ac97db38d6a74b1a0b06
Vladislav <vladisl13777@gmail.com>
2022-05-20 10:50:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+// let a = confirm('Hello guys');
+// console.log(a);
+
+const answer = prompt('Сколько вам лет ?', ''); 
+console.log(typeof(answer)); // Вся инфа, которая идет от пользователя - это есть строка
+document.write('lddlasdl');
